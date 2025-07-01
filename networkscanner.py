cbbf7da5cb4def62f92adf53da4d9900a520660f
Yash Bhootra <66923200+2smooth4u@users.noreply.github.com>
2025-07-01 18:51:12 +0530

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -1,12 +1,27 @@
+# Import all modules from scapy (used for packet crafting and sniffing)
 from scapy.all import *
 
+# Define the network interface to use for sending packets (e.g., eth0)
 interface = "eth0"
+
+# Define the IP range to scan using ARP (CIDR notation for a /24 subnet)
 ip_range = "192.168.17.0/24"
+
+# Define the broadcast MAC address (used to broadcast ARP requests)
 broadcastMac = "ff:ff:ff:ff:ff:ff"
 
-packet = Ether(dst=broadcastMac)/ARP(pdst  = ip_range)
+# Craft an Ethernet packet with destination set to broadcast MAC
+# and stack it with an ARP request for the target IP range
+packet = Ether(dst=broadcastMac)/ARP(pdst=ip_range)
 
-ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
+# Send the packet and receive responses:
+# - srp() sends and receives Ethernet frames at Layer 2
+# - timeout=2 seconds wait per packet
+# - iface specifies which interface to use
+# - inter=0.1 sets delay between each packet
+ans, unans = srp(packet, timeout=2, iface=interface, inter=0.1)
 
-for send,receive in ans:
-         print (receive.sprintf(r"%Ether.src% - %ARP.src%"))
+# Loop through each received response
+for send, receive in ans:
+    # Print the MAC and IP address of each host that responded
+    print(receive.sprintf(r"%Ether.src% - %ARP.src%"))
