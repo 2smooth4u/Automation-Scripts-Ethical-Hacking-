556ef1d953571e3e9cf2fa985c9e5af7497db9e9
Yash Bhootra <66923200+2smooth4u@users.noreply.github.com>
2025-07-01 18:46:29 +0530

    Update filedownloader.py

--- filedownloader.py
+++ filedownloader.py
@@ -1,5 +1,11 @@
+# Import the 'requests' library to handle HTTP requests
 import requests
 
-url =  'https://download.sysinternal.com/files/PSTools.zip '
-r = requests.get(url , allow_redirects=True)
-open('PSTools.zip' , 'wb').write(r.content)
+# Define the URL of the file to be downloaded
+url = 'https://download.sysinternal.com/files/PSTools.zip'
+
+# Send a GET request to the URL and allow automatic redirection (if any)
+r = requests.get(url, allow_redirects=True)
+
+# Write the downloaded content (binary data) to a local file named 'PSTools.zip'
+open('PSTools.zip', 'wb').write(r.content)
